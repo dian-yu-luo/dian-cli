6a13d1c60615ac79cafbdf2910e717ed4f160cb7
dianyuluo <1739556970@qq.com>
2023-03-22 06:31:14 +0800

    添加了spdlog库

--- hello_test.cc
+++ hello_test.cc
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <nlohmann/json.hpp>
 #include <fstream>
+#include <spdlog/spdlog.h>
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions)
 {
@@ -26,4 +27,28 @@ TEST(HelloTest, json)
     data["list"] = "我真牛逼";
     fi << data;
     fi.close();
+}
+
+TEST(HelloTest, testspdlog)
+{
+    system("chcp 65001");
+    spdlog::info("我的大兄弟啊");
+    spdlog::error("Some error message with arg: {}", 1);
+
+    spdlog::warn("Easy padding in numbers like {:08d}", 12);
+    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    spdlog::info("Support for floats {:03.2f}", 1.23456);
+    spdlog::info("Positional args are {1} {0}..", "too", "supported");
+    spdlog::info("{:<30}", "left aligned");
+
+    spdlog::set_level(spdlog::level::debug); // Set global log level to debug
+    spdlog::debug("This message should be displayed..");
+
+    // change log pattern
+    spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+
+    // Compile time log levels
+    // define SPDLOG_ACTIVE_LEVEL to desired level
+    SPDLOG_TRACE("Some trace message with param {}", 42);
+    SPDLOG_DEBUG("Some debug message");
 }
