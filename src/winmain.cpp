270e638684de0655c2c204bcd12ccda1b0f6898e
dianyuluo <1739556970@qq.com>
2023-03-22 21:59:53 +0800

    最好的分平台是选用两个平台

---
+++ src/winmain.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+void initwin()
+{
+    system("chcp 65001 > nul");
+}
+
+int main(int argc, char const *argv[])
+{
+    initwin();
+    
+    
+
+    return 0;
+}
