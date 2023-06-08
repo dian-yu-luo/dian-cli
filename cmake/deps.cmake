6c9722646388a9df4bf5021d922cd4e278f7eaf5
dianyuluo <1739556970@qq.com>
2023-06-09 05:35:59 +0800

    sqlite dep

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -27,3 +27,12 @@ FetchContent_Declare(
   GIT_REPOSITORY https://github.com/gabime/spdlog.git
   GIT_TAG v1.9.2)
 FetchContent_MakeAvailable(spdlog)
+
+FetchContent_Declare(
+  SQLiteCpp
+  GIT_REPOSITORY https://github.com/SRombauts/SQLiteCpp.git
+  GIT_TAG master
+  )
+set(SQLITECPP_RUN_CPPLINT OFF CACHE BOOL "")
+
+FetchContent_MakeAvailable(SQLiteCpp)
