992402ecf2d57259f6f20a83c43f077b5d4959ea
dianyuluo <1739556970@qq.com>
2025-05-24 21:52:38 +0800

    用脚本构建引入依赖,构建项目,只需要构建一次就可以了

--- build.bat
+++ build.bat
@@ -1,5 +1,16 @@
 @REM cmake 构建项目
 @echo off
-REM 设置构建目录
-cmake  -S. -Bbuild  
-cmake --build build --target ALL_BUILD
+
+if not exist vcpkg (
+    git clone https://github.com/microsoft/vcpkg.git
+)
+cd vcpkg 
+if not exist vcpkg.exe (
+    call bootstrap-vcpkg.bat -disableMetrics || echo bootstrap-vcpkg.bat run with error, go on ...
+)
+.\vcpkg.exe install 
+.\vcpkg.exe export --raw --output-dir .. --output=vcpkg-export
+rmdir /s /q ..\vcpkg_installed
+cd .. 
+@REM cmake  -S. -Bbuild  
+@REM cmake --build build --target ALL_BUILD
